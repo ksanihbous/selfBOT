715ffb839427d21f7819d6247424d56d65c13795
PH-13 <31429243+PHIEZUKE@users.noreply.github.com>
2018-08-12 17:32:39 +0700

    Update config.py

--- LineAPI/linepy/config.py
+++ LineAPI/linepy/config.py
@@ -29,10 +29,10 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '8.9.1'
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'PH-13'
-    SYSTEM_VER  = '10.12.0'
+    SYSTEM_VER  = '1'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
